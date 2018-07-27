1e4e1a818723da7d419b0e96fb3b47ef25827ec0
sbimochan <bmochan@gmail.com>
2018-07-27 13:11:08 +0545

    change namings, indentations, remove unnecessary method

--- __init__.py
+++ __init__.py
@@ -6,14 +6,14 @@ __version__ = '0.1.0'
 
 from sys import argv
 
-standupMsg = (str(argv[1]))
-lines = standupMsg.split("\n")
+standup_msg = (argv[1])
+lines = standup_msg.split("\n")
 separated_lines = []
 
 for line in reversed(lines):
-  line_without_hyphen = line.split("-")
-  separated_lines.append(line_without_hyphen[1])
+    line_without_hyphen = line.split("-")
+    separated_lines.append(line_without_hyphen[1])
 for counter, separated_line in enumerate(separated_lines):
-  line_without_parenthesis = separated_line.split("(")
-  result = str(counter+1) + ")" + line_without_parenthesis[0]
-  print(result)
+    line_without_parenthesis = separated_line.split("(")
+    result = str(counter+1) + ")" + line_without_parenthesis[0]
+    print(result)
